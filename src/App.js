d7b04c530f07624c58a1eb0df70674700e380d99
Poonwannadocoding <worawit.pen@student.mahidol.edu>
2024-02-14 01:19:08 +0700

    up to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
+
+  const [answer, setAnswer] = useState(0);
+  const [isHoveredYes, setIsHoveredYes] = useState(false);
+  const [isHoveredNo, setIsHoveredNo] = useState(false);
+
+  const handleButtonClick = (response) => {
+    setAnswer(response);
+  };
+
+  const handleMouseEnterYes = () => {
+    setIsHoveredYes(true);
+  };
+
+  const handleMouseLeaveYes = () => {
+    setIsHoveredYes(false);
+  };
+
+  const handleMouseEnterNo = () => {
+    setIsHoveredNo(true);
+  };
+
+  const handleMouseLeaveNo = () => {
+    setIsHoveredNo(false);
+  };
+
+  const imageSize = { height: 400, width: 400 }; // Set the desired image size
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {answer === 1 ? (
+          <img src={require('./img/cat3.gif')} alt="cat3" style={imageSize} />
+        ) : answer === 2 ? (
+          <img src={require('./img/cat2.gif')} alt="cat2" style={imageSize} />
+        ) : (
+          <img
+            src={isHoveredYes ? require('./img/cat3.gif') : isHoveredNo ? require('./img/cat2.gif') : require('./img/cat1.jpg')}
+            alt={isHoveredYes ? "cat3" : isHoveredNo ? "cat2" : "cat1"}
+            style={imageSize}
+          />
+        )}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Will You Be My Valentine?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <button 
+          onClick={
+            () => handleButtonClick(1)}
+            onMouseEnter={handleMouseEnterYes}
+            onMouseLeave={handleMouseLeaveYes}
+            
+            >
+              Yes
+            </button>
+          <button onClick={() => handleButtonClick(2)}
+          onMouseEnter={handleMouseEnterNo}
+          onMouseLeave={handleMouseLeaveNo}
+          >
+            No
+            </button>
+        </div>
       </header>
     </div>
   );
 }
+
 
 export default App;
